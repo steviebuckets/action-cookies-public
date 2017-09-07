06a4e0d131d36665799761677f3daaf814e27a37
steviebuckets <steviebuckets33@gmail.com>
2017-09-07 13:36:35 -0700

    upated with new footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,10 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="mastfoot" id="contact-footer">
+    <div className="outer">
+      <p>Welcome to the ACTION!
+        <br/>We are so happy to hear from our customers and we love to get orders too!</p>
+    </div>
       <div className="inner">
         <h1>Contact</h1>
         <p>actioncookies@gmail.com</p>
@@ -15,10 +19,6 @@ export default function Footer() {
         <a href="https://www.instagram.com/actioncookies/">
           <i className="fa fa-instagram fa-lg" aria-hidden="true"></i>
         </a>
-      </div>
-      <div className="outer">
-        <p>Welcome to the ACTION!
-          <br/>We are so happy to hear from our customers and we love to get orders too!</p>
       </div>
     </div>
   )
